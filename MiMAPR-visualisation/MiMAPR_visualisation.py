64a204ac4241d8e4b2705ab91de28e76aa9ab7d3
NovokshanovEvgeny <evgeny.novokshanov@gmail.com>
2023-04-28 10:29:34 +0300

    Добавлен проект на языке Python для визуализации полученных данных.

---
+++ MiMAPR-visualisation/MiMAPR_visualisation.py
@@ -0,0 +1,18 @@
+
+
+
+def main():
+    f = open("../output.txt", "r")
+    temp_grid = []
+    i = 0
+    for line in f:
+        i += 1; 
+        data = line.split()
+
+        print(data)
+        if(i > 0):
+
+
+
+if __name__ == "__main__":
+    main()
